9f992c43bb9ef0a9022e4bee466ef6454f31fabe
Jesús Roberto <18680131@cuautla.tecnm.mx>
2023-03-14 13:09:09 -0600

    cambios realizados por JR

--- src/Componentes/ComponenteJesus.jsx
+++ src/Componentes/ComponenteJesus.jsx
@@ -1,7 +1,40 @@
 
+
 function ComponenteJesus() {
   return (
-    <div>ComponenteJesus</div>
+    <div className="mas">
+
+    <h2 className="nombreJ">Jesús Naranjo</h2>
+    <h2 className="cosas">Cosas que me gustan</h2>
+    
+    <div className="container">
+        <div className="yo">
+            <h3>Yo</h3>
+            <img className="imagenJ" src='https://scontent.fcvj2-1.fna.fbcdn.net/v/t1.18169-9/18614_802955283133361_226632276040551269_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=cdbe9c&_nc_eui2=AeGQUzD0Ep_92iNtV6x9Je8q5Gmj2M9dMNjkaaPYz10w2Nwq0CB6XvCsvcEALjoIvmPeqhWdkDXjGOFh5y1aKh-D&_nc_ohc=qBJLpZ8keEsAX83n2KU&_nc_ht=scontent.fcvj2-1.fna&oh=00_AfDOop4nh2Gpdq5yod0xO8I-8H-_ReZdWYiAEE_A7V71_g&oe=64381D65' alt=""/>
+            <p>Mi nombre es Jesús Roberto, aquí no sabia usar los filtros así que solo me veo cenizo y sin gracia :v</p>
+        </div>
+        <div className="oneP">
+            <h3>Anime</h3>
+            <img className="imagenJ" src="https://www.tonica.la/__export/1669231086602/sites/debate/img/2022/11/23/one-piece.jpg_242310155.jpg" alt=""/>
+            <p>Mi anime favorito es One Piece, se que tiene muchos capitulos y a las personas les da miedo verlo pero, cuando comienzas no hay vuelta atras y eso es lo mejor, por que te atrapa por su buena historia.</p>
+        </div>
+        <div className="barca">
+            <h3>Equipo de Futbol</h3>
+            <img className="imagenJ" src="https://pbs.twimg.com/media/FdMC3X_XkAEro3G.jpg" alt=""/>
+            <p>El FC Barcelona es mi equipo favorito y es por el que me comenzo a gustar el futbol, recuerdo que fue en el año 2006 cuando conoci este equipo.</p>
+        </div>
+        <div className="gusto">
+            <h3>Gusto</h3>
+            <img className="imagenJ" src="https://scontent.fcvj2-1.fna.fbcdn.net/v/t1.18169-9/539673_589985564395085_1739312317_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeF708cZtfY3sbxWUgJHPYC5LL4UsqETPkgsvhSyoRM-SIUXcKbvUH8-wejEKTZdxAJ1D9_sytrxB0sJSlupUb0p&_nc_ohc=gpZ7eJL7y9MAX-0waRI&_nc_ht=scontent.fcvj2-1.fna&oh=00_AfDVruoCEgRZc1YuAbcYLI3Ju2nJ_9Tqq6h-jP4aAG2xAA&oe=6438244E" alt=""/>
+            <p>Me gusta mucho la música, incluso aprendí a tocar guitarra, bateria y bajo, estuve dentro de una banda, estabamos comenzando y nos llamabamos The Screams Of Aria.</p>
+        </div>
+        <div className="Music">
+            <h3>Música</h3>
+            <img className="imagenJ" src="https://images.sk-static.com/images/media/profile_images/artists/2497826/huge_avatar" alt=""/>
+            <p>Esta es una muy buena banda, tiene buenas rolitas, de igual manera me gusta mucho la música que esta relacionadad al metal, rock,e indie. </p>
+        </div>
+    </div>
+    </div>
   )
 }
 
